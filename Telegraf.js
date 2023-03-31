a13764222451fc54505063fbd86a91b30cd286bc
omprakashTk <109216475+omprakashTk@users.noreply.github.com>
2023-03-31 11:16:06 +0530

    Create Telegraf.js

---
+++ Telegraf.js
@@ -0,0 +1,12 @@
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply('Welcome!'));
+
+bot.help((ctx) => ctx.reply('Send me a message and I will echo it back to you.'));
+
+bot.on('text', (ctx) => ctx.reply(`You said: "${ctx.message.text}"`));
+
+bot.launch();
+
